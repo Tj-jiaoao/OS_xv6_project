c2a160a934e39f7ddd5d698d24ee5613c16b040e
Tj-jiaoao <2050780@tongji.edu.cn>
2023-08-08 10:53:03 +0800

    update

--- kernel/proc.c
+++ kernel/proc.c
@@ -663,9 +663,7 @@ uint64
 count_process(void) { // added function for counting used process slots (lab2)
   uint64 cnt = 0;
   for(struct proc *p = proc; p < &proc[NPROC]; p++) {
-    // acquire(&p->lock);
-    // 不需要锁进程 proc 结构，因为我们只需要读取进程列表，不需要写
-    if(p->state != UNUSED) { // 不是 UNUSED 的进程位，就是已经分配的
+    if(p->state != UNUSED) { 
         cnt++;
     }
   }
