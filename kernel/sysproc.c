47af953a086069b9d4d502c7876d2670a8f21819
Tj_Jiaoao <2050780@tongji.edu.cn>
2023-08-16 07:52:37 -0700

    updateAlarm

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -108,3 +108,25 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+uint64
+sys_sigalarm(void){
+  int ticks;
+  uint64 handler;
+  if (argint(0, &ticks) < 0)  return -1;
+  if (argaddr(1, &handler) < 0) return -1;
+
+  struct proc *p = myproc();
+  p->ticks = ticks;
+  p->handler = handler;
+  p->ticks_cnt = 0;
+  return 0;
+}
+
+uint64
+sys_sigreturn(void){
+  struct proc *p = myproc();
+  *(p->trapframe) = *(p->trapframe_backup);
+  p->handlering = 0;
+  return 0;
+}
