0e4821d768c0df9f5562656697c7783912427410
Tj_Jiaoao <2050780@tongji.edu.cn>
2023-08-17 21:23:08 -0700

    UpdateThread

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,7 +30,16 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  
+  pthread_mutex_lock(&bstate.barrier_mutex);
+  bstate.nthread += 1;
+  if (bstate.nthread < nthread){
+    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
+  }else{
+    bstate.nthread = 0;
+    bstate.round += 1;  //You should increment bstate.round each time all threads have reached the barrier
+    pthread_cond_broadcast(&bstate.barrier_cond);
+  }
+  pthread_mutex_unlock(&bstate.barrier_mutex);
 }
 
 static void *
